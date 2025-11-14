ad6c87a64f0ee374df58f5128ab0620b76ea63c5
d4n13lpro <cdap785@gmail.com>
2025-11-13 21:55:26 -0500

    Add main.py with 2 GET routes and enable CORS

--- server-flask/main.py
+++ server-flask/main.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():    
     return 'Hello world'
 
+@app.route('/api/users')
+def get_users():
+    return {
+        'users': ['Alice', 'Bob', 'Charlie']
+    }
+@app.route('/api/fruits')
+def get_fruits():
+    return ['Apple', 'Banana', 'Cherry']
 
 
 if __name__ == '__main__':
