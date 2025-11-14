2aaf575c0ba6bef6850fe05d074f6fc5073cb24d
d4n13lpro <cdap785@gmail.com>
2025-11-13 22:36:19 -0500

    adicionado objeto

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+type User = {
+  id: number;
+  name: string;
+  last_name: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data.users)); // Ajusté para que funcione con el formato esperado
+  }, []); // Añadí el arreglo vacío para que se ejecute solo una vez al montar el componente
 
   return (
     <>
@@ -16,20 +28,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React with Vite + Python with Flask</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h2>Users</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server-flask/main.py
+++ server-flask/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 from flask_cors import CORS
 
@@ -5,20 +6,36 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def hello_world():    
+def hello_world():
     return 'Hello world'
 
 @app.route('/api/users')
 def get_users():
     return {
-        'users': ['Alice', 'Bob', 'Charlie']
+        'users': [
+            {
+                'id': 1,
+                'name': 'Alice',
+                'last_name': 'Perez'
+            },
+            {
+                'id': 2,
+                'name': 'Bob',
+                'last_name': 'Patiño'
+            },
+            {
+                'id': 3,
+                'name': 'Carlos',
+                'last_name': 'Acuña'
+            }
+        ]
     }
+
 @app.route('/api/fruits')
 def get_fruits():
-    return ['Apple', 'Banana', 'Cherry']
-
+    return {
+        'fruits': ['Apple', 'Banana', 'Cherry']
+    }
 
 if __name__ == '__main__':
-    app.run(debug=True) # by default Flask runs on port 5000
-    
-    
+    app.run(debug=True)
